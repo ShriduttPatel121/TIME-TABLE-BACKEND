7ab080e1ad03f850117c5115719a71da60edfcfd
shridutt <shridutt270@gmail.com>
2021-06-29 16:02:44 +0530

    added jwt check for protected routes

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const userController = require('../controllers/user-controller');
+const checkAuth = require('../middlewares/auth');
 
 
 const router = express.Router();
+
+router.use(checkAuth);
 
 router.post('/addUser', userController.addUser);
 
